a56ae4e80a9177dd02354e59be9e18fc78b0296d
Nguyễn Minh Châu <81848005+NguyenMinhChau@users.noreply.github.com>
2021-10-07 10:31:44 +0700

    Lab03

--- Lab03/Lab03/Controllers/EmployeeController.cs
+++ Lab03/Lab03/Controllers/EmployeeController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Lab03.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +10,43 @@ namespace Lab03.Controllers
 {
     public class EmployeeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Index(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                // save
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Generator()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Generator(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                // save
+            }
+            return View();
+        }
+
+
+
+
         public IActionResult CheckEmployeeNo(string EmployeeNo)
         {
-            if(EmployeeNo == "777")
+            string[] empNos = new string[] { "777", "222", "555" };
+            if(empNos.Contains(EmployeeNo))
             {
                 return Json(data: "Mã này đã có");
             }
@@ -22,6 +54,39 @@ namespace Lab03.Controllers
             {
                 return Json(data: true);
             }
+        }
+        
+        // Sync
+        public IActionResult DemoSync()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var demo = new Employee();
+            demo.Test01();
+            demo.Test02();
+            sw.Stop();
+            return Content($"Chạy hết {sw.ElapsedMilliseconds}ms");
+        }
+        // Async
+        public async Task<IActionResult> DemoAsyncs()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var demo = new Employee();
+            var x = demo.Test01Async();
+            var  y = demo.Test02Async();
+            var z = demo.Test03Async();
+            await x;
+            await y;
+            await z;
+            sw.Stop();
+            return Content($"Chạy hết {sw.ElapsedMilliseconds}ms");
+        }
+
+        //Razor: tham khảo thêm tại W3School
+        public IActionResult DemoRazor()
+        {
+            return View();
         }
     }
 }
--- Lab03/Lab03/Models/Employee.cs
+++ Lab03/Lab03/Models/Employee.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lab03.Models
@@ -10,60 +11,121 @@ namespace Lab03.Models
     public enum Gender { Male,Femal}
     public class Employee
     {
-        public int ID { get; set; }
+        public Guid? ID { get; set; }
+
+
         [Display(Name = "Mã nhân viên")]
         [Remote(action: "CheckEmployeeNo", controller: "Employee")]
         public string EmployeeNo { get; set; }
-        [Display(Name = "Họtên")]
+
+
+        [Display(Name = "Họ tên")]
         [MinLength(3, ErrorMessage = "Tối thiểu 3 kí tự")] 
         [StringLength(100)]
         public string FullName { get; set; }
+
+
         [Display(Name = "Email")] 
         [EmailAddress]
+        [Required]
         public string Email { get; set; }
+
+
         [Display(Name = "Website")] 
         [Url]
         public string Website { get; set; }
+
         [Display(Name = "Ngày sinh")]
-        [BirthDateCheck]
+        //[BirthDateCheck]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+
+
         [Display(Name = "Giới tính")] 
-        public Gender Gender { get; set; }
+        public Gender? Gender { get; set; }
+
+        
         [Display(Name = "Lương")] 
         [Range(0, 10000000)] 
         public double Salary { get; set; }
+
+
         [Display(Name = "Bán thời gian")] 
         public bool IsPartTime { get; set; }
+
+
         [Display(Name = "Địa chỉ")] 
         [RegularExpression("[a-zA-Z 0-9]*")] 
         public string Address { get; set; }
+
+    
         [Display(Name = "Điện thoại")] 
-        [RegularExpression("0[3789][0-9]{8}")] 
+        [RegularExpression("0[35789][0-9]{8}")] 
         public string Phone { get; set; }
-        [Display(Name = "Sốtài khoản")] 
+
+        
+        [Display(Name = "Số tài khoản")] 
+        // Sử dụng 1 trong 2
         [CreditCard] 
-        [DataType(DataType.CreditCard)] 
+        //[DataType(DataType.CreditCard)] 
         public string CreditCard { get; set; }
+
+
         [Display(Name = "Thông tin thêm")] 
         [DataType(DataType.MultilineText)] 
         public string Description { get; set; }
+
+
+
         public class BirthDateCheckAttribute : ValidationAttribute
         {
             public BirthDateCheckAttribute() : base("Ngày sinh không hợp lệ") { }
             protected override ValidationResult IsValid(object value, ValidationContext validationContext)
             {
-                var model = validationContext.ObjectInstance as Employee;
-                if(model == null)
+                try
                 {
-                    throw new ArgumentException("Tham số truyền vào không đúng");
+                    var dob = (DateTime)value;
+                    if (dob > DateTime.Now)
+                    {
+                        return new ValidationResult("Ngày sinh không hợp lệ");
+                    }
+                    return ValidationResult.Success;
                 }
-                if(DateTime.Now.Year - model.BirthDate.Year < 10)
+                catch
                 {
-                    return new ValidationResult("Chưa đủ 10 tuổi");
+                    return new ValidationResult("Ngày sinh không hợp lệ");
                 }
-                return ValidationResult.Success;
             }
+        }
+
+
+        // Sync
+        public int Test01()
+        {
+            Thread.Sleep(2000);
+            return 10;
+        }
+        public string Test02()
+        {
+            Thread.Sleep(3000);
+            return "hello";
+        }
+        // Async
+        public async Task<int> Test01Async()
+        {
+            await Task.Delay(2000);
+            return 10;
+        }
+        public async Task<string> Test02Async()
+        {
+            await Task.Delay(3000);
+            return "hello";
+        }
+        // kiểu null
+        public async Task Test03Async()
+        {
+            await Task.Delay(4000);
+            return;
         }
     }
 }
