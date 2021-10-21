a6f27d00e1be7e08eccc687c10e9acefbaf3b9ab
Nguyễn Minh Châu <81848005+NguyenMinhChau@users.noreply.github.com>
2021-10-21 07:06:11 +0700

    Lab05

---
+++ Lab05/Lab05/Models/Loai.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab05.Models
+{
+    public class Loai
+    {
+        [Key] public int MaLoai { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string TenLoai { get; set; }
+        public string MoTa
+        { get; set; }
+        public string Hinh
+        { get; set; }
+    }
+}
---
+++ Lab05/Lab05/Models/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FreeSql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab05.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Loai> Loai { get; set; }
+        public DbSet<HangHoa> HangHoa { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=EFCoreCodeFirst-QLBH;Integrated Security=True;");
+        }
+    }
+}
