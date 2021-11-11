50ec317c430dac770c46896cb63429aff1481c94
Nguyễn Minh Châu <81848005+NguyenMinhChau@users.noreply.github.com>
2021-11-11 08:01:29 +0700

    DemoAuthen

---
+++ DmoAuthen/DmoAuthen/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DmoAuthen.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
