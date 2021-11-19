541d3187da31aa71f3b779f0cc45e149652de34d
Nguyễn Minh Châu <81848005+NguyenMinhChau@users.noreply.github.com>
2021-11-19 17:06:03 +0700

    CodeFirst

---
+++ MyAppCodeFirst/MyAppCodeFirst/Migrations/20211119100337_DbCodeFirstTwo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyAppCodeFirst.Migrations
+{
+    public partial class DbCodeFirstTwo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
